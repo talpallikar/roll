ba247b56ae58247996b85907e94288645315a12b
arjun <arjuntalpallikar@utexas.edu>
2017-06-13 16:49:57 -0400

    initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 offset; 
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - player.transform.position; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //LateUpdate runs every frame after all items are processed in update
+    private void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+    public float speed;
+    public Text countText;
+    public Text winText; 
+
+    private Rigidbody rb;
+    private int count; 
+
+	// Use this for initialization
+	void Start ()
+    {
+
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = ""; 
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    // Fixed update is called for physics calcs
+    void FixedUpdate()
+    {
+        float moveHor = Input.GetAxis("Horizontal");
+        float moveVer = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHor, 0.0f, moveVer);
+
+        rb.AddForce (movement * speed); 
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            SetCountText(); 
+        }
+    }
+    void SetCountText()
+    {
+        countText.text = "count: " + count.ToString();
+        if (count >= 8)
+        {
+            winText.text = "You Win!"; 
+        }
+    }
+}
